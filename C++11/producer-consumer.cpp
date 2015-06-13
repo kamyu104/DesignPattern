cb14326667077856ed272273a9149ed12f9c0711
Allen Liu <kamyu104@gmail.com>
2015-06-13 23:48:54 +0800

    update

--- C++11/producer-consumer.cpp
+++ C++11/producer-consumer.cpp
@@ -1,3 +1,5 @@
+// Copyright (c) 2015 kamyu. All rights reserved.
+
 #include <iostream>
 #include <queue>
 #include <thread>
@@ -18,23 +20,29 @@ using std::this_thread::sleep_for;
 
 template<typename T>
 class SyncQueue {
-public:
+ public:
     void put(const T& val) {
         unique_lock<mutex> lock{mtx};
+        // If the condition is not met,
+        // wait until the thread is notified,
+        // the mutex is acquirable, and the condition is met.
         cond.wait(lock, [this]() { return q.size() != q_size; });
         q.emplace(val);
-        cond.notify_one();
-    }
-    
-    void get(T& val) {
+        cond.notify_one();  // Notify one of the waiting thread.
+    }  // Unlock the mutex.
+
+    void get(T *val) {
         unique_lock<mutex> lock{mtx};
+        // If the condition is not met,
+        // wait until the thread is notified,
+        // the mutex is acquirable, and the condition is met.
         cond.wait(lock, [this]() { return !q.empty(); });
-        val = q.front();
+        *val = q.front();
         q.pop();
-        cond.notify_one();
-    }
-    
-private:
+        cond.notify_one();  // Notify one of the waiting thread.
+    }  // Unlock the mutex.
+
+ private:
     mutex mtx;
     condition_variable cond;
     queue<T> q;
@@ -42,10 +50,10 @@ private:
 };
 
 class Producer {
-public:
-    Producer(SyncQueue<int>& q, mutex& cout_mtx) :
-             q_(q), cout_mtx_(cout_mtx) {}
-    
+ public:
+    Producer(SyncQueue<int> *q, mutex *cout_mtx) :
+             q_(*q), cout_mtx_(*cout_mtx) {}
+
     void run() {
         for (int i = 0; i < 100; ++i) {
             const int num = i;
@@ -58,18 +66,19 @@ public:
         }
     }
 
-private:
+ private:
     SyncQueue<int>& q_;
     mutex& cout_mtx_;
 };
 
 class Consumer {
-public:
-    Consumer(SyncQueue<int>& q, mutex& cout_mtx) : q_(q), cout_mtx_(cout_mtx) {}
+ public:
+    Consumer(SyncQueue<int> *q, mutex *cout_mtx) :
+             q_(*q), cout_mtx_(*cout_mtx) {}
     void run() {
         for (int i = 0; i < 100; ++i) {
             int num;
-            q_.get(num);
+            q_.get(&num);
             {
                 lock_guard<mutex> lock(cout_mtx_);
                 cout << "Consumed: " << num << endl;
@@ -78,7 +87,7 @@ public:
         }
     }
 
-private:
+ private:
     SyncQueue<int>& q_;
     mutex& cout_mtx_;
 };
@@ -86,14 +95,14 @@ private:
 int main() {
     SyncQueue<int> q;
     mutex cout_mtx;
-    Producer p{q, cout_mtx};
-    Consumer c{q, cout_mtx};
+    Producer p{&q, &cout_mtx};
+    Consumer c{&q, &cout_mtx};
 
     thread producer_thread{&Producer::run, &p};
     thread consumer_thread{&Consumer::run, &c};
 
     producer_thread.join();
     consumer_thread.join();
-    
+
     return 0;
 }
